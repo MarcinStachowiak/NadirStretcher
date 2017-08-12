d22ae3f15299b1ae26b00ce93a17ee76b52fee59
Marcin Stachowiak <marcin.stachowiak.ms@gmail.com>
2017-08-12 21:46:47 +0200

    Added blurring in accordance with the shape

--- Stretcher.py
+++ Stretcher.py
@@ -1,11 +1,13 @@
 import os
 import cv2
 import glob
+import numpy as np
 
-blur_height=722;
+
 blur_thickness=5;
-gauss=51
-rand_gauss=6
+gauss=1
+offset=8;
+blur_threshold=8
 show=False
 
 path_to_input_folder = input('Ścieżka do folderu ze zdjęciami: ')
@@ -26,25 +28,37 @@ for i, path in enumerate(jpg_files):
     img=cv2.imread(path)
     i_h=img.shape[0]
     i_w=img.shape[1]
-    img_min=img[(i_h-blur_height):(i_h-blur_height+blur_thickness),:,:]
-    img_min_resize=cv2.resize(img_min, (i_w, blur_height))
-    img_min_resize_gauss=cv2.GaussianBlur(img_min_resize,(gauss,gauss),0)
+    layer=np.zeros([i_h,i_w,3],dtype=img.dtype)
 
+    for i in range(0,i_w-1):
+        for j in range(i_h-1,0,-1):
+            if(np.any(np.array(img[j,i,:])>blur_threshold)):
+                img_min=img[(j-offset-blur_thickness):(j-offset),i,:]
+                img_min_resize = cv2.resize(img_min, (3,(i_h-j)+offset+blur_thickness))
+                layer[(j-offset-blur_thickness):i_h,i,:]=img_min_resize
+                break
 
-    img_result=img
-    img_result[i_h-blur_height:i_h,:,:]=img_min_resize_gauss
-    img_result[i_h-blur_height-rand_gauss:i_h-blur_height+rand_gauss]=cv2.GaussianBlur(img_result[i_h-blur_height-rand_gauss:i_h-blur_height+rand_gauss],(5,5),0)
+    r,thresh = cv2.threshold(layer, 1, 1, cv2.THRESH_BINARY)
+    layer_gauss = cv2.GaussianBlur(layer, (gauss, gauss), 0)
+    layer_gauss=layer_gauss*thresh
+
+    result_img=img
+    for i in range(0,i_w-1):
+        for j in range(0,i_h):
+            if(np.any(np.array(layer_gauss[j,i,:])!=0)):
+                result_img[j:i_h,i,:]=layer_gauss[j:i_h,i,:]
+                break
 
     output_path=os.path.join(path_to_output_folder, file_name +"_blur"+ ".jpg")
-    cv2.imwrite(output_path, img_result, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+    cv2.imwrite(output_path, result_img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
     if show:
         cv2.namedWindow(file_name,cv2.WINDOW_NORMAL)
-        cv2.imshow(file_name,img_result)
+        cv2.imshow(file_name,result_img)
 
 if show:
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-print("_____________________")
+print("_____________________")m
 print("Zakończono z SUKCESEM")
